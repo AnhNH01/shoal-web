54ffe8f3bc4fb9778bc1d31d117890808869e3be
AnhNH01 <haianh552@gmail.com>
2023-11-30 21:22:56 +0700

    init nuxt project with NuxtUI

---
+++ pages/auth/register.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>This is register page</h1>
+</template>
+
+<script setup lang="ts"></script>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+  <UButton loading label="Hello button"></UButton>
+</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
